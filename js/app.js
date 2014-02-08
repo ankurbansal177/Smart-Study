ce95639d4867c0afcc99814002e3e6aa536b8b22
ankurbansal177 <ankurbansal177@gmail.com>
2014-02-08 17:46:52 +0530

    ddw

--- js/app.js
+++ js/app.js
@@ -11,11 +11,11 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/about', {templateUrl: 'about.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/home', {templateUrl: 'homepage.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/home', {templateUrl: 'homePage.html', controller: 'MyCtrl2'});
   $routeProvider.when('/site', {templateUrl: 'index.html', controller: 'MyCtrl2'});
   $routeProvider.when('/contact', {templateUrl: 'contact.html', controller: 'MyCtrl2'});
   $routeProvider.when('/blog', {templateUrl: 'blog.html', controller: 'MyCtrl2'});
   $routeProvider.when('/check', {templateUrl: 'check.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/', {templateUrl: 'homepage.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/', {templateUrl: 'homePage.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -9,5 +9,7 @@ angular.module('myApp.controllers', []).
   .controller('MyCtrl2', [function() {
 
   }])
-  .controller('SuperCtrl', [function($scope) {
+  .controller('SuperCtrl', ['$scope',function($scope) {
+        $scope.title = 'hello there';
+        $scope.companyName = 'Angular Technologies Pvt. Ltd. 2013';
   }]);
