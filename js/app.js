6abcae26a9932f0f047cd82051bb7145c357b59f
ankurbansal177 <ankurbansal177@gmail.com>
2014-02-08 16:35:20 +0530

    first

---
+++ js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/about', {templateUrl: 'about.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/home', {templateUrl: 'homepage.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/site', {templateUrl: 'index.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/contact', {templateUrl: 'contact.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/blog', {templateUrl: 'blog.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/check', {templateUrl: 'check.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/', {templateUrl: 'homepage.html', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/'});
+}]);
